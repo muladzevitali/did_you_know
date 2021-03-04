da0f267818dea4bb3fc62d9b17b03a8e12c44584
muladzevitali <muladzevitali@gmail.com>
2021-03-04 21:10:56 +0400

    urls changed

--- backend/did_you_know/urls.py
+++ backend/did_you_know/urls.py
@@ -13,6 +13,6 @@ schema_view = get_schema_view(openapi_info, public=True, permission_classes=[per
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.authentication.urls')),
+    path('rest/', include('apps.authentication.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
